19d42de469e2884dda2e56be5f7a595ca20fc472
RexingRui <mjlzzzc@163.com>
2019-01-04 21:25:39 +0800

    complete follow add and moving

--- src/components/common/customFollow.vue
+++ src/components/common/customFollow.vue
@@ -19,23 +19,30 @@
             <div class="move-box">
                 <div class="custom-followed">
                     <ul>
-                        <li v-for="item in followed" :key="item.id">
-                            <span class="iconfont">&#xe6df;</span>
-                            {{item.content}}
-                        </li>
-                        <li v-if="followedMoving"></li>
+                        <div  v-for="item in followed" :key="item.id">
+                            <li
+                                @mousedown="handleMouseDown(item, $event)"
+                                 class="item-follow"
+                                 :class="{'li-move': eleMoving}"
+                            >
+                                <span class="iconfont">&#xe6df;</span>
+                                {{item.content}}
+                            </li>
+                            <li v-if="item.back"></li>
+                        </div>
                     </ul>
                 </div>
                 <div class="custom-unfollowed">
                     <ul>
-                        <li
-                            v-for="item in unfollowed"
-                            :key="item.id"
-                            @mousedown="handleMouseDown"
-                            :class="{ 'li-move': eleMoving }"
-                        >
-                            <span class="iconfont">&#xe6df;</span>{{item.content}}</li>
-                        <li v-if="unfollowedMoving"></li>
+                        <div  v-for="item in unfollowed" :key="item.id">
+                            <li
+                                @mousedown="handleMouseDown(item, $event)"
+                                 class="item-follow"
+                                 :class="{'li-move': eleMoving}"
+                            >
+                                <span class="iconfont">&#xe6df;</span>{{item.content}}</li>
+                            <li v-if="showBack" class="back-frame"></li>
+                        </div>
                     </ul>
                 </div>
             </div>
@@ -47,18 +54,22 @@ export default {
     name: "customFollow",
     data() {
         return {
-			moveSelf: {},
+            moveSelf: {},
+            moveItem: {},
             mouseX: 0,
             mouseY: 0,
+            itemX: 0,
+            itemY: 0,
             show: false,
+            showBack: false,
             changeColor: false,
-            followedMoving: false,
-            unfollowedMoving: false,
             eleMoving: false,
-            followed: [{ id: 1, content: "我的导航" }],
+            backContainer: false,
+            moveFag: false,
+            followed: [{ id: "followed-00", content: "我的导航", back: false }],
             unfollowed: [
-                { id: 1, content: "我的股票" },
-                { id: 2, content: "我的小说" }
+                { id: "unfollowed-10", content: "我的股票", back: false },
+                { id: "unfollowed-11", content: "我的小说", back: false }
             ]
         };
     },
@@ -86,25 +97,89 @@ export default {
         handleClose() {
             this.show = false;
         },
-        handleMouseDown(e) {
+        handleMouseDown(item, e) {
+            // 将移动的对象返回给data
+            this.moveItem = item;
+            // 目标的初始位置
+            this.eleMoving = true;
+            this.itemX = e.target.offsetLeft;
+            this.itemY = e.target.offsetTop;
+            // 鼠标的初始位置
             this.mouseX = e.clientX;
             this.mouseY = e.clientY;
 			this.liMoving = true;
-			this.moveSelf = e.target;
+            this.moveSelf = e.target;
+            this.showBack = true;
         },
         handleMouseMove(e) {
             if (this.liMoving) {
-                this.moveSelf.style.top = e.clientY - this.mouseY + "px";
-                this.moveSelf.style.left = e.clientX - this.mouseX + "px";
+                var movingY = this.itemY + e.clientY - this.mouseY;
+                var movingX = this.itemX + e.clientX - this.mouseX;
+                // 限定在指定的区域内移动
+                if (movingY > 110) {
+                    movingY = 110;
+                } else if (movingY < 0) {
+                    movingY = 0;
+                }
+                if (movingX > 570) {
+                    movingX = 570;
+                } else if (movingX < 0) {
+                    movingX = 0;
+                }
+                // 虚线框的位置变化
+                if (movingX > 235) {
+                    this.showBack = true;
+                    this.moveFag = true;
+                } else {
+                    this.showBack = false;
+                }
+
+                if (movingX < 235 && this.moveFag === true) {
+                        var _this = this;
+                        this.unfollowed = this.unfollowed.filter(function (value, index, arr) {
+                            return value.id !== _this.moveItem.id;
+                        });
+                        this.unfollowed.push(this.moveItem);
+                }
+
+                this.moveSelf.style.top =  movingY+ "px";
+                this.moveSelf.style.left =  movingX+ "px";
             }
         },
         handleMouseUp(e) {
-			this.moveSelf.style.top = "0px";
-			this.moveSelf.style.left = "0px";
-			this.moveSelf.style.top = "auto";
-			this.moveSelf.style.left = "auto";
-			this.liMoving  = false;
-			this.moveSelf = null;
+            if (this.moveItem) {
+                // 移动目标当前的偏移量
+                var offsetX = parseInt(this.moveSelf.style.left.replace("px", ""));
+                    // 判断移动目标属于那一类
+                    // 移动的是未关注项目
+                if(this.moveItem.id.search("unfollowed") !== -1) {
+                    // 判断是否进入指定区域
+                    console.log(offsetX);
+                    if (offsetX < 235) {
+                    // 将未关注改为关注
+                        var _this = this;
+                        this.unfollowed = this.unfollowed.filter(function (value, index, arr) {
+                            return value.id !== _this.moveItem.id;
+                        });
+                        // 修改id
+                        this.moveItem.id = this.moveItem.id.replace("un", "");
+                        // 添加到关注项中
+                        this.followed.push(this.moveItem);
+                    }
+                }
+                // 将目标改为相对定位
+                this.eleMoving = false;
+                // 放回原位置或添加的位置
+                this.moveSelf.style.top = "0px";
+                this.moveSelf.style.left = "0px";
+                // 取消定位，为了二次点击时定位到上次移动的最后位置
+                this.moveSelf.style.top = "auto";
+                this.moveSelf.style.left = "auto";
+                this.liMoving  = false;
+                this.showBack = false;
+                this.moveSelf = null;
+                this.moveItem = null;
+            }
         }
     }
 };
@@ -162,19 +237,28 @@ export default {
             height: 60%;
             border: 1px solid gray;
             margin-left: 4%;
-            div {
+            .custom-followed, .custom-unfollowed {
                 width: 280px;
                 border: 1px solid red;
                 ul {
-                    display: flex;
                     width: 100%;
+                    display: flex;
                     li {
                         width: 76px;
+                        height: 20px;
                         text-align: center;
                         margin-right: 3px;
                         position: relative;
                         cursor: pointer;
                         user-select: none;
+                    }
+                    .li-move {
+                        position: absolute !important;
+                        z-index: 200;
+                    }
+                    .back-frame {
+                         z-index: 10;
+                        border:  1px dashed #38f;
                     }
                 }
             }
@@ -182,9 +266,9 @@ export default {
                 li {
                     border: 1px dashed #c1c1c1;
                     &:hover {
-                        border-color: #398bfb;
+                        border-color: #38f;
                         span {
-                            background: #398bfb;
+                            background: #38f;
                         }
                     }
                     span {
@@ -200,18 +284,16 @@ export default {
                 }
             }
             .custom-unfollowed {
-                li {
+                .item-follow {
+                     z-index: 100;
                     border: 1px solid #c1c1c1;
                     span {
                         color: #d1d1d1;
                     }
                     &:hover {
-                        background: #398bfb;
-                        border-color: #398bfb;
+                        background: #38f;
+                        border-color: #38f;
                     }
-                }
-                .li-move {
-                    position: relative;
                 }
             }
         }
